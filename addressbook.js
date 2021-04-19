0df95e986d8f8b11fac60de927901902f662044c
Deep Saha <deepsaha288@yahoo.com>
2021-04-19 23:31:26 +0530

    duplicate check done on person name

--- addressbook.js
+++ addressbook.js
@@ -10,11 +10,11 @@ class AddressBook{
 
     constructor(...params){
 
-        let regexname = new RegExp('[A-Z]{1}[a-z]{3,}');
-        let regexaddress = new RegExp('[A-Z]{1}[a-z]{4,}');
-        let regexpincode = RegExp('^[1-9]{1}[0-9]{2}[\\s]?[0-9]{3}$');
-        let regexphone = RegExp(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im);
-        let emailregex = RegExp('^[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-])*@[A-Za-z0-9-]+(?:\\.[A-Za-z0-9-]+)*$');
+        const regexname = new RegExp('[A-Z]{1}[a-z]{3,}');
+        const regexaddress = new RegExp('[A-Z]{1}[a-z]{4,}');
+        const regexpincode = RegExp('^[1-9]{1}[0-9]{2}[\\s]?[0-9]{3}$');
+        const regexphone = RegExp(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im);
+        const emailregex = RegExp('^[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-])*@[A-Za-z0-9-]+(?:\\.[A-Za-z0-9-]+)*$');
 
         if( regexname.test(params[0]) && regexname.test(params[1]) ){
             this.firstname = params[0];
@@ -62,15 +62,24 @@ let addressBook = [] ;
 try{
     let contact1 = new AddressBook("Mark","Robinson","Malibu","Venice","California",100019,9019291971,"mark@gmail.com");
     let contact2 = new AddressBook("Robert","Downey","Broklyn","Manhattan","Newyork",300013,8918192191,"robert@gmail.com");
-    let contact3 = new AddressBook("Chirs","Davidson","Saltlake","Saltlakecity","Texas",400018,8891823349,"chris@gmail.com");
+    let contact3 = new AddressBook("Chris","Davidson","Saltlake","Saltlakecity","Texas",400018,8891823349,"chris@gmail.com");
     let contact4 = new AddressBook("Keem","Star","Capital","Phoenix","Arizona",866019,7827826798,"keem@gmail.com");
+    let contact5 = new AddressBook("Chris","Seagule","Panther","Panama","Texas",400712,7672789892,"mathew@gmail.com");
 
     addressBook.push(contact1); 
     addressBook.push(contact2);
     addressBook.push(contact3);
     addressBook.push(contact4);
 
+    let arr= addressBook.map( ele => ele.getfirstname === contact5.getfirstname );
+    console.log(arr);
+
+    if(arr.includes(true)){
+        console.log("Contact already exits please enter new contact");
+    }else{ addressBook.push(contact5); }
+    
     console.log(addressBook);
+    
     let countArr = addressBook.map(ele => typeof ele.getfirstname === 'string');
     let sum = countArr.reduce((prev, curr)=> prev + curr); 
     console.log("Size of array before deltion: "+sum);
@@ -89,3 +98,9 @@ try{
 }catch(e){
     console.error(e);
 }
+
+console.log(addressBook);
+
+let countArr = addressBook.map(ele => typeof ele.getfirstname === 'string');
+let sum = countArr.reduce((prev, curr)=> prev + curr); 
+console.log("Size of array after deltion: "+sum);
